cc3a4cdbf1c98133f76b64b4d56a4b763e03937e
angelacachog <a-cachogarcia@kellogg.northwestern.edu>
2016-02-07 16:40:34 -0600

    wer

--- international.js
+++ international.js
@@ -5,4 +5,18 @@ $(function() {
     $("#chart").show(10);
   });
 
+  $("#do-something2").on("click", function(event) {
+    event.preventDefault();
+    $("#chart2").show(10);
+  });
+
+  $("#do-something3").on("click", function(event) {
+    event.preventDefault();
+    $("#chart3").show(10);
+  });
+
+    $("#do-something4").on("click", function(event) {
+    event.preventDefault();
+    $("#chart4").show(10);
+  });
 })
